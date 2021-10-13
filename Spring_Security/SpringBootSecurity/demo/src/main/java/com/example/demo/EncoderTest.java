556a81f6e81eafc9b02ca29d0cbb274865261555
minnim6 <mi_jeong_o@naver.com>
2021-10-13 20:23:50 +0900

    pass encoder 수정

--- Spring_Security/SpringBootSecurity/demo/src/main/java/com/example/demo/EncoderTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-public class EncoderTest {
-    PasswordEncoder passwordEncoder =
-            PasswordEncoderFactories.createDelegatingPasswordEncoder();
-}
---
+++ Spring_Security/SpringBootSecurity/demo/src/test/java/com/example/demo/EncoderTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.*;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@SpringBootTest
+public class EncoderTest {
+
+    @Test
+    public void createDelegatingPasswordEncoder() {
+        String idForEncode = "bcrypt";
+        Map<String, PasswordEncoder> encoders = new HashMap();
+        encoders.put(idForEncode, new BCryptPasswordEncoder());
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        encoders.put("sha256", new StandardPasswordEncoder());
+        PasswordEncoder passwordEncoder =  new DelegatingPasswordEncoder(idForEncode, encoders);
+        log.info(passwordEncoder.encode("password"));
+    }
+
+
+}
