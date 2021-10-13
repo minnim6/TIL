dcd790fbf18bcdb47a7793d387947a06d1642cdd
minnim6 <mi_jeong_o@naver.com>
2021-10-14 01:30:57 +0900

    10.14

--- Spring_Security/SpringBootSecurity/demo/src/main/java/com/example/demo/WebSecurityConfig.java
+++ Spring_Security/SpringBootSecurity/demo/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,15 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth){
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(User
+                .withUsername("user")
+                .password("password123")
+                .roles("USER")
+                .build()
+        );
     }
 }
--- Spring_Security/SpringBootSecurity/demo/src/test/java/com/example/demo/EncoderTest.java
+++ Spring_Security/SpringBootSecurity/demo/src/test/java/com/example/demo/EncoderTest.java
@@ -3,9 +3,12 @@ package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.*;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.swing.*;
 import java.util.HashMap;
@@ -14,6 +17,12 @@ import java.util.Map;
 @Slf4j
 @SpringBootTest
 public class EncoderTest {
+
+    @Autowired
+    InMemoryUserDetailsManager inMemoryUserDetailsManager;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Test
     public void createDelegatingPasswordEncoder() {
@@ -33,9 +42,13 @@ public class EncoderTest {
     }
 
     @Test
-    public void passwordEncoderTest(){
-
+    public void getPassword(){
+        UserDetails user = inMemoryUserDetailsManager.loadUserByUsername("user");
+        log.info(user.getPassword());
+        log.info(user.getUsername());
     }
-
-
+    @Test
+    public void passwordTest(){
+        log.info(passwordEncoder.encode("password"));
+    }
 }
